55f07973cd51d80fefe769ea10f4ef46f9589dc8
jack-lijing <lijing48301243@gmail.com>
2014-12-12 17:09:05 +0800

    home

--- slide.js
+++ slide.js
@@ -8,9 +8,7 @@
  */
 
 
-function $(element){
-  return document.getElementById(element);
-}
+
 /*
 funciton bind(){
   var pre=$("pre");
@@ -20,24 +18,49 @@ funciton bind(){
 
 }
   */
-//window.onload=bind;
-//var pshow=$("pfirst");
-//alert(pshow);
+
+var index=0;
+var stop;
+function showPic(){
+  pclick(index);
+  index++;
+  if(index==3)index=0;
+  stop=setTimeout("showPic()", 1000);
+}
+
+function pclick(index){
+   clearTimeout(stop);
+    var slide=$("slide");
+    var pic=$("#slide img");
+    var a=$("a");
+    for(i=0; i<3;i++)
+    {
+     pic[i].setAttribute("class","hide");
+     a[i].setAttribute("class",""); 
+    };
+  
+   
+//    $(this).setAttribute("class","on");
+  pic[index].setAttribute("class","show");
+  a[index].setAttribute("class","on"); 
+}
+
+function slider(){
+  setTimeout("silder(index)", 1000);
+}
+
+
+
 window.onload=function(){
   var i=0;
-  var p=$("pre");
-  p.onclick=function(){
-    var slide=$("slide");
-    var sp=slide.getElementsByTagName("div");
-    sp[i].setAttribute("class","hide");
-    i =(i+1)%3;
-    sp[i].setAttribute("class","show");
+  var p=$("a");
+  showPic();
+ // p.onclick=pclick;
     /*
     pfirst=$("pfirst");
     alert("class"+pfirst.getAttribute("class"));
     pfirst.setAttribute("class","hide", 1);
     alert("class"+$("psecond").getAttribute("class"));
     $("psecond").setAttribute("class","show", 1);
-    */
-  };
+   */
 };
